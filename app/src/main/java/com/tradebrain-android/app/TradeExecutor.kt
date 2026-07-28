f14a796b88009c91985b3f2b3d3090d0d464e4fc
honeydewnuts-pixel <honeydewnuts@gmail.com>
2026-07-28 23:43:00 +0100

    Create TradeExecutor.kt

---
+++ app/src/main/java/com/tradebrain-android/app/TradeExecutor.kt
@@ -0,0 +1,9 @@
+package com.tradebrain.app
+object TradeExecutor {
+    fun executeTrade(signal: String){
+        when(signal){
+            "BUY" -> { /* Code to click BUY in MT4 app using Accessibility */ }
+            "SELL" -> { /* Code to click SELL in MT4 app using Accessibility */ }
+        }
+    }
+}
